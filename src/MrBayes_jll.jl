b20b809ccd49ba96e568e2e194f362a274fbb193
jlbuild <juliabuildbot@gmail.com>
2022-04-25 21:22:33 +0000

    MrBayes_jll build 3.2.7+0

---
+++ src/MrBayes_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule MrBayes_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("MrBayes")
+JLLWrappers.@generate_main_file("MrBayes", UUID("4028151b-0928-560d-8573-53f5e508f174"))
+end  # module MrBayes_jll
